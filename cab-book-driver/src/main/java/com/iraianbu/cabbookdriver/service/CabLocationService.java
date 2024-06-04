567a07afe0289df7d3bcf49e12c5c5ac4b05d433
Iraianbu-11 <iraianbu011@gmail.com>
2024-06-04 20:47:54 +0530

    Initial Commit

---
+++ cab-book-driver/src/main/java/com/iraianbu/cabbookdriver/service/CabLocationService.java
@@ -0,0 +1,17 @@
+package com.iraianbu.cabbookdriver.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CabLocationService {
+    @Autowired
+    private KafkaTemplate<String,Object> kafkaTemplate;
+
+    public void updateLocation(String location){
+        kafkaTemplate.send("cab-location",location);
+    }
+
+}
